7157606e494b52a37cfaaf6d85fa8d2d72a47e77
convinto <convinto@yandex.ru>
2024-07-03 23:23:07 +0300

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -1,5 +1,6 @@
 immutable_var = 1, 2, 'a', 'b'
 print('Immutable tuple:', immutable_var)
+#   immutable_var[1] = 3
 mutable_list = [1, 2, "a", "b", "c"]
 mutable_list [4]="Modified"
 print("Mutable list:", mutable_list)
